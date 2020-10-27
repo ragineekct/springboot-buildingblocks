7e473c58f58a5aee6602d9f21c98217eae40afc0
Raginee Rathore <raginee.kct@gmail.com>
2020-10-27 11:41:11 +0530

    second commit

--- src/main/java/com/stacksimplyfy/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplyfy/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplyfy.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
